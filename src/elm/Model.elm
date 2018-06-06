423788be3e6aed398979e8a550b81a416d25326e
leojpod <leo@repositive.io>
2018-06-06 20:36:16 +0100

    write up the definition of our model

--- src/elm/Model.elm
+++ src/elm/Model.elm
@@ -1,5 +1,40 @@
-module Model exposing (Model)
+module Model exposing (Model, CountDownState(..), initModel)
+
+import Time
 
 
 type alias Model =
-    {}
+    { config : Config
+    , countdownState : CountDownState
+    }
+
+
+{-| Represent the state of the current countdown.
+
+  - Pause: The Maybe will be used to see if the countdown has been placed in pause (i.e. there is a state to restore) or just stopped (Nothing to restore)
+  - Ticking: The boolean will indicate if the speaker is in time or running overtime
+
+-}
+type CountDownState
+    = Paused (Maybe CountDownState)
+    | Ticking Bool Time.Time
+
+
+type alias Config =
+    { speachTime : Time.Time
+    , allowedOverTime : Time.Time
+    }
+
+
+basicConfig : Config
+basicConfig =
+    { speachTime = 30.0 * Time.second
+    , allowedOverTime = 5.0 * Time.second
+    }
+
+
+initModel : Model
+initModel =
+    { config = basicConfig
+    , countdownState = Paused Nothing
+    }
