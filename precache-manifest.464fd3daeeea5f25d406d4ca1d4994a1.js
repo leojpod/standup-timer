6a9b9bac98250bcbb1c7480d6100e54ee2649020
Leo jPod <leo.jpod@lilo.org>
2020-02-14 12:10:06 +0100

    Updates

---
+++ precache-manifest.464fd3daeeea5f25d406d4ca1d4994a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b684849e0eeff0e9bd53f78f48c822cd",
+    "url": "/audio/flat-line-beep.mp3"
+  },
+  {
+    "revision": "29f628d5c064f71e742723f867c2742e",
+    "url": "/audio/short-beep.mp3"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "69139fd193177519776aa4f6eeae5feb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "94545f35437686ea28e7",
+    "url": "/static/css/main.cc310fc9.chunk.css"
+  },
+  {
+    "revision": "94545f35437686ea28e7",
+    "url": "/static/js/main.94545f35.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
